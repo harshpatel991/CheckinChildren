7989bae9a193f7e3290546f659cfadf529be3866
Olzhas Alipov <alipov@illinois.edu>
2015-02-14 12:38:52 -0600

    fixed constructor and renamed the file

--- php/model/createManagerModel.php
+++ php/model/createManagerModel.php
@@ -5,6 +5,6 @@ class createManagersModel{
 
     public function __construct($_name, $_facilityId){
         $name = $_name;
-        $_facilityId = $_facilityId;
+        $facilityId = $_facilityId;
     }
 }
