73ab0dd98acbb004d2af03369a90d201bd93fe01
pianoajd <pianoajdunk@gmail.com>
2015-02-15 15:20:40 -0600

    Created beginnings of the employee DAO and model.

--- scripts/models/dao/employeeDao.php
+++ scripts/models/dao/employeeDao.php
@@ -10,22 +10,42 @@ class employeeDao {
 
     //TODO: Use cache to reduce DB calls.
     private static $employeeCache = array();
-    private $connection;
 
     public function __construct()
     {
+
+    }
+
+    public function find($id){
         $connection = DbConnectionFactory::create();
+        $query = "SELECT * FROM employee WHERE id=:id";
+
+        $stmt=$this->$connection->prepare($query);
+        $stmt->bindParam(':id', $id);
+
+        $result=$stmt->execute();
+
+        $connection=null;
+
+        return new employeeModel($result);
     }
 
-    public function find($id)
-    {
-        $query = "t";
-        $this->$connection->prepare($query);
-        $this->$connection->execute($query);
+    public function create_employee($emp_info){
+        $connection = DbConnectionFactory::create();
+
+        $query = "INSERT INTO user (pass, roll) VALUES(:pass, 'employee')";
+        $stmt=$connection->prepare($query);
+        $stmt->bindParam(':pass', $emp_info[0]);
+
+        $stmt->execute();
+        $id=$stmt->lastInsertId();
+
+
     }
 
-    public function save($user){
-        $query = "";
+    private function insert($employee){
+        $connection = DbConnectionFactory::create();
+        $query = "INSERT INTO employee ";
         $this->$connection->prepare($query);
         $this->$connection->execute($query);
     }
