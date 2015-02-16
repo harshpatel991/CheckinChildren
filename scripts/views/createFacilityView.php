df0af5b8cae30430c5628c57a14ba63434e0d5fb
Harsh <hpatel90@illinos.edu>
2015-02-15 19:45:14 -0600

    Added createFacility.php, facilityDAO, createFacilityView

---
+++ scripts/views/createFacilityView.php
@@ -0,0 +1,12 @@
+<?php
+    //load the html template and fill in the appropriate values
+    $htmlFileLocation = dirname(__FILE__).'/../../html//createFacility.html';
+
+    $template = file_get_contents ($htmlFileLocation);
+
+    $companyId = '50';//TODO: get the company id from session variable -->$_SESSION["id"];
+    $template = str_replace("LOGGED_IN_COMPANY_ID", $companyId, $template);
+
+    //print out html
+
+    echo $template;
