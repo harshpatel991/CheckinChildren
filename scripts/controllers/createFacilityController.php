6c0c223bc8c573312fd745aaab16ed7a762e0c5f
Harsh <hpatel90@illinos.edu>
2015-02-15 21:25:06 -0600

    Removed used files from 1st pair meeting

--- scripts/controllers/createFacilityController.php
+++ scripts/controllers/createFacilityController.php
@@ -2,22 +2,19 @@
 
 require_once(dirname(__FILE__).'/../models/dao/facilityDAO.php');
 
-//Read in POST data
-//if(!empty($_POST['submitted'])) {
+//Read in POST data from form
 
-    $facilityModel = new facilityModel($_POST['company_id'], $_POST['address'], $_POST['phone_number']);
+$facilityModel = new facilityModel($_POST['company_id'], $_POST['address'], $_POST['phone_number']);
 
-    if ($facilityModel->isValid()) {
-        $facilityDAO = new FacilityDAO();
-        $facility_id = $facilityDAO->insert($facilityModel);
+if ($facilityModel->isValid()) {
+    $facilityDAO = new FacilityDAO();
+    $facility_id = $facilityDAO->insert($facilityModel);
 
-        header("Location: ../../public/facilities.php?facility_id=".$facility_id); //send browser to the page for newly created facility
-        exit();
+    header("Location: ../../public/facilities.php?facility_id=".$facility_id); //send browser to the page for newly created facility
+    exit();
 
-    } else {
-        //redirect to employee creation page with error message
-        header("Location: ../../public/companyHome.php"); /* Redirect browser */
-        exit();
-    }
-
-//}
+} else {
+    //redirect to employee creation page with error message
+    header("Location: ../../public/companyHome.php"); /* Redirect browser */
+    exit();
+}
