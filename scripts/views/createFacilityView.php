04af887d5b8bd651fead1d5fc7695c55c8b51ac1
Harsh Patel <hpatel90@illinois.edu>
2015-02-16 10:28:48 -0600

    Create facility view bug fix

--- scripts/views/createFacilityView.php
+++ scripts/views/createFacilityView.php
@@ -1,6 +1,6 @@
 <?php
     //load the html template and fill in the appropriate values
-    $htmlFileLocation = dirname(__FILE__).'/../../html//createFacility.html';
+    $htmlFileLocation = dirname(__FILE__).'/../../html/createFacility.html';
 
     $template = file_get_contents ($htmlFileLocation);
 
