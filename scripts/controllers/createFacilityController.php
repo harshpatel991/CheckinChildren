6e39c3a423ba1b8f33952f4b5cf281608bb4066b
Harsh <hpatel90@illinos.edu>
2015-02-16 23:41:07 -0600

    Added pages for viewing all company facilities, added page for viewing single facility, added link on index page

--- scripts/controllers/createFacilityController.php
+++ scripts/controllers/createFacilityController.php
@@ -3,14 +3,14 @@
 require_once(dirname(__FILE__).'/../models/dao/facilityDAO.php');
 
 //Read in POST data from form
-
-$facility = new facilityModel($_POST['company_id'], $_POST['address'], $_POST['phone_number']);
+$company_id = $_COOKIE[cookieManager::$userId];
+$facility = new facilityModel($company_id, $_POST['address'], $_POST['phone_number']);
 
 if ($facility->isValid()) {
     $facilityDAO = new FacilityDAO();
     $facility_id = $facilityDAO->insert($facility);
 
-    header("Location: ../../public/displayFacilitiesView.php?facility_id=".$facility_id); //send browser to the page for newly created facility
+    header("Location: ../../public/displayFacilities.php?facility_id=".$facility_id); //send browser to the page for newly created facility
     exit();
 
 } else { //redirect to employee creation page with error message
--- scripts/views/createFacilityView.php
+++ scripts/views/createFacilityView.php
@@ -4,10 +4,6 @@
 $htmlFileLocation = dirname(__FILE__).'/../../html/createFacility.html';
 $template = file_get_contents ($htmlFileLocation);
 
-$companyId = '50';//TODO: get the company id from session variable -->$_SESSION["id"];
-
-$template = str_replace("LOGGED_IN_COMPANY_ID", $companyId, $template);
-
 if(isset($_GET['error'])) {
     if($_GET['error'] == 1) {
         $template = str_replace("ERROR_MESSAGE", "Invalid information", $template);
