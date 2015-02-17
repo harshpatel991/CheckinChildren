73726f3be27467445280993ab286b619e56a6dfc
Harsh <hpatel90@illinos.edu>
2015-02-16 22:49:59 -0600

    Added tests for facility

--- scripts/models/dao/facilityDAO.php
+++ scripts/models/dao/facilityDAO.php
@@ -9,33 +9,6 @@ require_once(dirname(__FILE__).'/../db/dbConnectionFactory.php');
 
 class FacilityDAO {
 
-    //Retrieves all facilities assoicated with a company
-    public function findFacilitiesInCompany($companyId) {
-
-        $connection = DbConnectionFactory::create();
-        $query = 'SELECT * FROM facility WHERE company_id=:company_id';
-        $stmt = $connection->prepare($query);
-        $stmt->bindParam(':company_id', $companyId);
-        $stmt->execute();
-        $facilities = $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'facilityModel');
-        $connection = null;
-
-        return $facilities;
-    }
-
-    //Retreives a facility matching the specified facility id
-    public function find($facility_id) {
-        $connection = DbConnectionFactory::create();
-        $query = 'SELECT * FROM facility WHERE facility_id=:facility_id';
-        $stmt = $connection->prepare($query);
-        $stmt->bindParam(':facility_id', $facility_id);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS, 'facilityModel');
-        $facility = $stmt->fetch();
-        $connection = null;
-        return $facility;
-    }
-
     //Inserts the specified facility in the data base
     public function insert(facilityModel $facility) {
         $connection = DbConnectionFactory::create();
@@ -48,5 +21,32 @@ class FacilityDAO {
         $facility_id = $connection->lastInsertId();
         $connection = null;
         return $facility_id;
+    }
+
+    //Retreives a facility matching the specified facility id
+    //Returns false if a facility is not found
+    public function find($facility_id) {
+        $connection = DbConnectionFactory::create();
+        $query = 'SELECT * FROM facility WHERE facility_id=:facility_id';
+        $stmt = $connection->prepare($query);
+        $stmt->bindParam(':facility_id', $facility_id);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'facilityModel');
+        $facility = $stmt->fetch();
+        $connection = null;
+        return $facility;
+    }
+
+    //Retrieves all facilities assoicated with a company
+    public function findFacilitiesInCompany($companyId) {
+        $connection = DbConnectionFactory::create();
+        $query = 'SELECT * FROM facility WHERE company_id=:company_id';
+        $stmt = $connection->prepare($query);
+        $stmt->bindParam(':company_id', $companyId);
+        $stmt->execute();
+        $facilities = $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'facilityModel');
+        $connection = null;
+
+        return $facilities;
     }
 }
