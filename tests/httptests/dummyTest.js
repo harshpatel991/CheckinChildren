de38ba28c8d6c71a91e95db4e8159e2eab9f9802
mwallic2 <mwallic@illinois.edu>
2015-02-16 23:39:19 -0600

    Added WebDriver for httptests

--- tests/httptests/dummyTest.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by matt.
- *
- * Dummy for commits
- *
- * We will be using Selenium testing framework in JavaScript for black box tests.
- * The framework will be added to the repo soon, with a sample test included.
- */
