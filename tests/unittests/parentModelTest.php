a43737425af320e1909015d13c5454c51e8d4553
nick <nicksamata@gmail.com>
2015-02-20 14:02:30 -0600

    added unittest for DAO and Model of parents

---
+++ tests/unittests/parentModelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+require_once(dirname(__FILE__).'/../../scripts/models/parentModel.php');
+
+class parentModelTest extends PHPUnit_Framework_TestCase {
+
+    public function testConstructor(){
+        $parent=new parentModel("Herbert", "pword", "test@test.com", "parent", "8008888989", "123 fake st", 999);
+
+        $this->assertEquals("Herbert", $parent->parent_name);
+        $this->assertEquals("pword", $parent->password);
+        $this->assertEquals("8008888989", $parent->phone);
+        $this->assertEquals("test@test.com", $parent->email);
+        $this->assertEquals("123 fake st", $parent->addr);
+        $this->assertEquals("parent", $parent->role);
+        $this->assertEquals(999, $parent->id);
+    }
+
+    public function testFilter(){
+        $parent=new parentModel("", "pword", "test@test.com", "parent", "8008888989", "123 fake st", 999);
+
+        $this->assertFalse($parent->isValid());
+
+        $parent->parent_name="Herbert";
+
+        $this->assertTrue($parent->isValid());
+
+        $parent->parent_name="kadjflkfjsdklfjsdlfkjakslfjkdlasfjklajsdflkdjfklasdfjdskajlfkajsfdlfkjdaslfjlasdfkja";
+
+        $this->assertFalse($parent->isValid());
+    }
+
+}
