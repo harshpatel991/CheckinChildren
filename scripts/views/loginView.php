73c39e69e809b0c37735662a1975291ace3f3ca2
Harsh Patel <hpatel90@illinois.edu>
2015-02-19 21:02:01 -0600

    Adding auto login links to login page

--- scripts/views/loginView.php
+++ scripts/views/loginView.php
@@ -1,10 +1,36 @@
+
+
+
 <h2>Login</h2>
 <form method="post" action="../scripts/controllers/loginController.php">
 
-    Email: <input type="text" name="email">
+    Email: <input type="text" name="email" id="email">
     <br><br>
-    Password: <input type="text" name="password">
+    Password: <input type="text" name="password" id="password">
     <br><br>
 
-    <input type="submit" name="submit" value="Login">
+    <input type="submit" name="submit" value="Login" id="submit">
 </form>
+
+
+------------LOGIN AS:------------<br>
+<a href="javascript:void(0)" onclick="autoFillUser(0);">Company - bigcompany1@gmail.com</a><br>
+<a href="javascript:void(0)" onclick="autoFillUser(1);">Manager - manager6@gmail.com</a><br>
+<a href="javascript:void(0)" onclick="autoFillUser(2);">Employee - baba_ganush2@gmail.com</a><br>
+<a href="javascript:void(0)" onclick="autoFillUser(3);">Parent - parent19@gmail.com</a><br>
+
+
+<script><!-- TODO: move this to js folder -->
+    var emails = ["bigcompany1@gmail.com", "manager6@gmail.com", "baba_ganush2@gmail.com", "parent19@gmail.com" ];
+    var passwords = ["password1", "password6", "password2", "password19"];
+
+    var emailBox=document.getElementById("email");
+    var passwordBox=document.getElementById("password");
+    var submitBox=document.getElementById("submit");
+
+    function autoFillUser(index) {
+        emailBox.value = emails[index];
+        passwordBox.value = passwords[index];
+        submitBox.click();
+    }
+</script>
