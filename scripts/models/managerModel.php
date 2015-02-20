d68b01c9759aafe924bb7f5b2aae0bba2512887a
Olzhas Alipov <alipov@illinois.edu>
2015-02-20 16:07:12 -0600

    deleted accessing facilityDao inside isValid()

--- scripts/models/managerModel.php
+++ scripts/models/managerModel.php
@@ -8,7 +8,7 @@ class managerModel extends userModel{
     public $facility_id;
     public $company_id;
 
-    public function __construct( $emp_name="", $password="", $facility_id=0, $company_id = 0, $email="", $role="", $id=0)
+    public function __construct( $emp_name="", $password="", $facility_id=0, $company_id = 0, $email="", $id=0)
     {
         $this->id=$id;
         $this->emp_name=$emp_name;
@@ -16,17 +16,10 @@ class managerModel extends userModel{
         $this->company_id = $company_id;
         $this->password=$password;
         $this->email=$email;
-        $this->role=$role;
+        $this->role = "manager";
     }
 
     public function isValid() {
-        $facilityDao = new facilityDAO();
-        $facility = $facilityDao->find($this->facility_id);
-
-        if ($facility == false || $facility->company_id != $this->company_id){
-            return false;
-        }
-
         if (strlen($this->emp_name)>30 || strlen($this->emp_name)<=0) {
             return false;
         }
