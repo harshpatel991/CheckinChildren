1ba7dde1eb7cc57fef753e0a5d6258aed1050c8b
Olzhas Alipov <alipov@illinois.edu>
2015-02-20 16:06:16 -0600

    check if company owns facility with facility_id

--- scripts/controllers/createManagerController.php
+++ scripts/controllers/createManagerController.php
@@ -3,14 +3,20 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require_once(dirname(__FILE__).'/../models/dao/managerDAO.php');
+require_once(dirname(__FILE__).'/../models/dao/facilityDAO.php');
 require_once(dirname(__FILE__).'/../models/managerModel.php');
 require_once(dirname(__FILE__).'/../cookieManager.php');
 
+
 $hashedPassword = managerModel::genHashPassword($_POST['password']);
+
+$facilityDao = new facilityDAO();
+$facility = $facilityDao->find($_POST['facility_id']);
+$valid = !($facility == false || $facility->company_id != $_COOKIE[cookieManager::$userId]);
 
 $manager=new managerModel($_POST['name'], $hashedPassword, $_POST['facility_id'], $_COOKIE[cookieManager::$userId], $_POST['email'], $_POST['role']);
 
-if ($manager->isValid()) {
+if ($manager->isValid() && $valid) {
     $managerDAO=new managerDAO();
     $managerDAO->createManager($manager);
 
