9a0f2e2a9809be70588842a49d088e94ed42923e
pianoajd <pianoajdunk@gmail.com>
2015-02-24 17:58:43 -0600

    Removed create Employeee.html and moved functionality to the create employee view.

--- scripts/views/createEmployeeView.php
+++ scripts/views/createEmployeeView.php
@@ -1,5 +1,16 @@
-<?php
-    $htmlFileLocation = dirname(__FILE__).'/../../html/createEmployee.html';
-
-    $template=file_get_contents ($htmlFileLocation);
-    echo $template;
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<form method="post" action="../scripts/controllers/form_handlers/createEmployeeFormHandler.php">
+    Name: <input type="text" name="name" > <br>
+    Employee Email: <input type="text" name="email"> <br>
+    Employee Password: <input type="text" name="password"><br>
+    <input type="hidden" name="role" value="employee">
+    <input type="submit" name="submit" value="Submit">
+</form>
+</body>
+</html>
