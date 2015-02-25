c0a3c4cf24da897c089186dce850fb73860343d4
Harsh <hpatel90@illinos.edu>
2015-02-24 20:53:40 -0600

    Fixed issues with creating a parent, company, and facility

--- scripts/views/createParentView.php
+++ scripts/views/createParentView.php
@@ -5,7 +5,7 @@
     <title></title>
 </head>
 <body>
-<form method="post" action="../scripts/controllers/createParentController.php">
+<form method="post" action="../scripts/controllers/form_handlers/createParentFormHandler.php">
     parent Name: <input type="text" name="name" > <br>
     Parent Email: <input type="text" name="email"> <br>
     Parent Phone Number: <input type="text" name="phone"> <br>
