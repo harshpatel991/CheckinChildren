63a71a15526eac55b240ec76c7e10fa3bc5c99ec
Harsh Patel <hpatel90@illinois.edu>
2015-02-26 15:23:46 -0600

    Adding bootstrap files, adding bootstrap imports to index page

--- public/index.php
+++ public/index.php
@@ -1,13 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: matt
- * Date: 2/16/15
- * Time: 5:03 AM
- */
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
-require_once(dirname(__FILE__).'/../scripts/controllers/authController.php');
-require_once(dirname(__FILE__).'/../scripts/views/indexView.php');
+    require_once(dirname(__FILE__).'/../scripts/controllers/authController.php');
 ?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+        <script src="js/bootstrap.js"></script>
+
+        <?php
+            require_once(dirname(__FILE__).'/../scripts/views/indexView.php');
+        ?>
+    </body>
+</html>
 
