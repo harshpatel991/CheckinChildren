daad3537df570e3e224bbd733791c1c1a4cda9bf
Harsh <hpatel90@illinos.edu>
2015-02-26 19:30:11 -0600

    Added files for editing a parent, not tested

---
+++ scripts/controllers/form_handlers/editParentFormHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../cookieManager.php');
+require_once(dirname(__FILE__) . '/../../models/dao/parentDAO.php');
+
+//Read in POST data from form
+$parent_id = $_COOKIE[cookieManager::$userId];
+$parent = new parentModel($_POST["parent_name"],"", $_POST["email"], $_POST["role"], $_POST["phone_number"], $_POST["address"], $parent_id);
+
+if ($parent->isValid()) {
+    $parentDAO = new ParentDAO();
+    $parentDAO->update($facility);
+
+    header("Location: ../../../public/displayParentInfo.php"); //send browser to the page for newly created facility
+    exit();
+
+} else { //redirect to employee creation page with error message
+    header("Location: ../../../public/editParent.php?error=1");
+    exit();
+}
