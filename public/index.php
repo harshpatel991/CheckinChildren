9a81e93ec6967081da6f286e707eb7b5fc985df0
Harsh <hpatel90@illinos.edu>
2015-02-26 23:03:32 -0600

    Modifying index for finalized bootstrap form

--- public/index.php
+++ public/index.php
@@ -4,15 +4,18 @@
 
 <!DOCTYPE html>
 <html>
-    <body>
+    <head>
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/bootstrap-theme.css">
         <script src="js/bootstrap.js"></script>
+    </head>
 
+    <body>
         <?php
             require_once(dirname(__FILE__).'/../scripts/views/indexView.php');
         ?>
     </body>
+
 </html>
 
