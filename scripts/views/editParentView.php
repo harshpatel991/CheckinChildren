a8ad95ce6dae0f8ccc17556339efa8746ced2416
Elzabad Kennedy <kenned45@illinois.edu>
2015-02-26 20:44:36 -0600

    Add the current info into the edit fields

--- scripts/views/editParentView.php
+++ scripts/views/editParentView.php
@@ -1,18 +1,23 @@
 <?php
+require_once(dirname(__FILE__).'/../controllers/authController.php');
+require_once(dirname(__FILE__).'/../models/dao/parentDAO.php');
+require_once(dirname(__FILE__).'/../models/parentModel.php');
 
+$parentDAO=new parentDAO();
+$parent=$parentDAO->find($_COOKIE[cookieManager::$userId]);
 
 ?>
 
 
 <form method="post" action="../scripts/controllers/form_handlers/editParentFormHandler.php">
     Name:
-    <input type="text" name="parent_name" > <br>
+    <input type="text" name="parent_name" value="<?php echo $parent->parent_name; ?>" > <br>
     Email:
-    <input type="text" name="email"><br>
+    <input type="text" name="email" value="<?php echo $parent->email; ?>"><br>
     Address:
-    <input type="text" name="address"> <br>
+    <input type="text" name="address" value="<?php echo $parent->address; ?>"> <br>
     Phone number:
-    <input type="text" name="phone_number"> <br>
+    <input type="text" name="phone_number" value="<?php echo $parent->phone_number; ?>"> <br>
 
     <input type="hidden" name="role" value="parent">
 
