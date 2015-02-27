0f5a2e14a9f0bf9a8ab20da12edbc12aca55fa95
Harsh Patel <hpatel90@illinois.edu>
2015-02-27 16:05:00 -0600

    Adding tests for viewing and editing children

--- scripts/views/editChildView.php
+++ scripts/views/editChildView.php
@@ -1,19 +1,26 @@
-
-<p style="color:red">
 <?php
+
+require_once(dirname(__FILE__).'/../models/dao/childDAO.php');
+require_once(dirname(__FILE__).'/../models/childModel.php');
+
+$childDAO=new childDAO();
+$child=$childDAO->find($_GET['child_id']);
+?>
+<p style="color:red">
+    <?php
     if(isset($_GET['error'])) {
         if($_GET['error'] == 1) {
             echo "Invalid Information";
         }
     }
-?>
+    ?>
 </p>
 
 <form method="post" action="../scripts/controllers/form_handlers/editChildFormHandler.php">
 Name:
-    <input type="text" name="child_name" > <br>
+    <input type="text" name="child_name" value="<?php echo $child->child_name; ?>"> <br>
 Allergies:
-    <input type="text" name="allergies"><br>
+    <input type="text" name="allergies" value="<?php echo $child->allergies; ?>"><br>
 
     <input type="hidden" name="child_id" value="<?php echo $_GET['child_id']; ?>">
     <input type="submit" value="Submit" name="submit">
