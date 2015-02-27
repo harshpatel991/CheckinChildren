9c9a63f873a5374b9c7927c1f49f3822d65d3dc2
Harsh <hpatel90@illinos.edu>
2015-02-27 16:05:00 -0600

    Adding tests for viewing and editing children

--- tests/httptests/tests/ParentTest.php
+++ tests/httptests/tests/ParentTest.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__).'/../SeleniumTestBase.php';
 require_once dirname(__FILE__).'/../TestMacros.php';
 
-class CompanyTest extends SeleniumTestBase
+class ParentTest extends SeleniumTestBase
 {
     public function setUp()
     {
@@ -58,6 +58,68 @@ class CompanyTest extends SeleniumTestBase
 
         $this->get_element("name=phone_number")->clear();
         $this->get_element("name=phone_number")->send_keys("12312378901");
+
+        $this->get_element("name=submit")->click();
+
+        $page = $this->driver->get_source();
+        $this->assertContains("Invalid Information", $page);
+    }
+
+    public function testViewChildren()
+    {
+        //Login first
+        testMacros::login($this->driver, "parent19@gmail.com", "password19");
+
+        $this->get_element("link=My Children")->click();
+
+        $page = $this->driver->get_source();
+        $this->assertContains("Ludvig Beetoven", $page);
+        $this->assertContains("Peter Parker", $page);
+
+        $this->get_element("link=Ludvig Beetoven")->click();
+
+        $page = $this->driver->get_source();
+        $this->assertContains("Ludvig Beetoven", $page);
+        $this->assertContains("4", $page);
+        $this->assertContains("Dogs", $page);
+    }
+
+    //Tests changing the name and allergies of a child
+    public function testEditChild()
+    {
+        //Login first
+        testMacros::login($this->driver, "parent19@gmail.com", "password19");
+
+        $this->get_element("link=My Children")->click();
+        $this->get_element("link=Ludvig Beetoven")->click();
+        $this->get_element("link=Edit This Child")->click();
+
+        $this->get_element("name=child_name")->clear();//clear input box
+        $this->get_element("name=child_name")->send_keys("New Beetoven");
+
+        $this->get_element("name=allergies")->clear();//clear input box
+        $this->get_element("name=allergies")->send_keys("None");
+
+        $this->get_element("name=submit")->click();
+
+        $page = $this->driver->get_source();
+        $this->assertContains("New Beetoven", $page);
+        $this->assertContains("None", $page);
+    }
+
+    //Tests changing child name to invalid value
+    public function testEditChildInvalid()
+    {
+        //Login first
+        testMacros::login($this->driver, "parent19@gmail.com", "password19");
+
+        $this->get_element("link=My Children")->click();
+        $this->get_element("link=Ludvig Beetoven")->click();
+        $this->get_element("link=Edit This Child")->click();
+
+        $this->get_element("name=child_name")->clear();//clear input box
+        $this->get_element("name=child_name")->send_keys("Ramalamashamalamadingdong Ganush");
+
 
         $this->get_element("name=submit")->click();
 
