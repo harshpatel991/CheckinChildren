178545141986ab2125050da09e641ce8ab5784dc
Harsh Patel <hpatel90@illinois.edu>
2015-02-28 13:51:06 -0600

    Adding bootstrap to all files before merge with 3D

--- public/displayEmployees.php
+++ public/displayEmployees.php
@@ -1,14 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 2/17/15
- * Time: 4:46 PM
- */
-error_reporting(E_ALL);
+error_reporting(E_ALL); //turn on error reporting
 ini_set("display_errors", 1);
 
-//Add require nav bar here
 require_once(dirname(__FILE__).'/../scripts/controllers/authController.php');
-require_once(dirname(__FILE__).'/../scripts/views/displayEmployeesView.php');
-//Add require footer here
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once(dirname(__FILE__).'/importBoostrap.php'); ?>
+    </head>
+
+    <body>
+        <?php require_once(dirname(__FILE__).'/../scripts/views/displayEmployeesView.php'); ?>
+    </body>
+</html>
+
--- public/index.php
+++ public/index.php
@@ -1,20 +1,18 @@
 <?php
+    error_reporting(E_ALL); //turn on error reporting
+    ini_set("display_errors", 1);
+
     require_once(dirname(__FILE__).'/../scripts/controllers/authController.php');
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.css">
-        <script src="js/bootstrap.js"></script>
+        <?php require_once(dirname(__FILE__).'/importBoostrap.php'); ?>
     </head>
 
     <body>
-        <?php
-            require_once(dirname(__FILE__).'/../scripts/views/indexView.php');
-        ?>
+        <?php require_once(dirname(__FILE__).'/../scripts/views/indexView.php'); ?>
     </body>
 
 </html>
