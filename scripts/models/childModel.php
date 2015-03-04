4406d047f49f37805a84869d8f60be47030111d5
mwallic2 <mwallic@illinois.edu>
2015-03-03 18:42:50 -0600

    Initial DB changes for checkin

--- scripts/models/childModel.php
+++ scripts/models/childModel.php
@@ -7,13 +7,20 @@ class childModel {
     var $child_name;
     var $allergies;
     var $facility_id;
+    var $last_checkin;
+    var $last_checkout;
+    var $expect_checkin;
 
-    function __construct($parent_id=0, $child_name="", $allergies="", $facility_id=0, $child_id=0) {
+    function __construct($parent_id=0, $child_name="", $allergies="",$facility_id=0, $child_id=0,
+                         $last_checkin=0, $last_checkout=0) {
         $this->parent_id = $parent_id;
         $this->child_name = $child_name;
         $this->allergies = $allergies;
         $this->facility_id = $facility_id;
         $this->child_id = $child_id;
+        $this->$last_checkin = $last_checkin;
+        $this->$last_checkout = $last_checkout;
+        $this->expect_checkin = [];
     }
 
     public function isValid() {
