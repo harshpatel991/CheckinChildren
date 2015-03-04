62bfac9ea16593b424ed6b6e7201a0aa7ba6cf96
Harsh <hpatel90@illinos.edu>
2015-03-03 20:03:32 -0600

    Adding bootstrap to public files that were missing it

--- public/createChild.php
+++ public/createChild.php
@@ -1,9 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nick
- * Date: 2/16/15
- * Time: 12:47 pM
- */
+error_reporting(E_ALL); //turn on error reporting
+ini_set("display_errors", 1);
+
 require_once(dirname(__FILE__).'/../scripts/controllers/authController.php');
-require_once(dirname(__FILE__).'/../scripts/views/createChildView.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once(dirname(__FILE__).'/importBoostrap.php'); ?>
+    </head>
+
+    <body>
+        <?php require_once(dirname(__FILE__).'/../scripts/views/createChildView.php');?>
+    </body>
+</html>
+
