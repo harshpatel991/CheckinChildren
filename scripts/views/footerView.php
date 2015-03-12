bad91f658c6b0ae121a7e434e619977ca3944776
Harsh <hpatel90@illinos.edu>
2015-03-12 18:24:06 -0500

    Home page design finished

--- scripts/views/footerView.php
+++ scripts/views/footerView.php
@@ -1,9 +1,11 @@
-<div class="row">
-    <div class="col-sm-12">
-        <footer class="custom-footer footer navbar-fixed-bottom">
-            <div class="container">
-                Copyright 2015 CheckinChildren
-            </div>
-        </footer>
+<div class="container-fluid">
+    <div class="row custom-footer">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <footer class=" footer">
+                <div class="container">
+                    Copyright 2015 CheckinChildren
+                </div>
+            </footer>
+        </div>
     </div>
 </div>
