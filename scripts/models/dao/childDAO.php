3edd4f4aad1a23f66e6404033aa7d788a04131c9
nick <nicksamata@gmail.com>
2015-03-13 17:59:17 -0500

    Edited Create Child Model, View, Controller(form handler), and DAO to allow children to be created with trusted parties

--- scripts/models/dao/childDAO.php
+++ scripts/models/dao/childDAO.php
@@ -8,13 +8,14 @@ class childDAO {
     function insert($child){
         $connection = DbConnectionFactory::create();
 
-        $query = "INSERT INTO child (child_id, parent_id, child_name, allergies, facility_id) VALUES ( :child_id, :parent_id, :child_name, :allergies, :facility_id)";
+        $query = "INSERT INTO child (child_id, parent_id, child_name, allergies, trusted_parties, facility_id) VALUES ( :child_id, :parent_id, :child_name, :allergies, :trusted_parties, :facility_id)";
         $stmt=$connection->prepare($query);
 
         $stmt->bindParam(":child_id", $child->child_id);
         $stmt->bindParam(":parent_id", $child->parent_id);
         $stmt->bindParam(":child_name", $child->child_name);
         $stmt->bindParam(":allergies", $child->allergies);
+        $stmt->bindParam(":trusted_parties", $child->trusted_parties);
         $stmt->bindParam(":facility_id", $child->facility_id);
 
         $stmt->execute();
@@ -36,6 +37,7 @@ class childDAO {
 
         $stmt->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'childModel');
         $child=$stmt->fetch();
+        $connection=null;
         $connection=null;
         if ($child!=false) {
             $child->expect_checkin = self::timesCsvToArray($child->expect_checkin);
