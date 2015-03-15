8d7abeb4c828436a02992a9afe94f1616e0828f9
pianoajd <pianoajdunk@gmail.com>
2015-03-15 17:54:45 -0500

    Changed placement of font api.

--- public/importBoostrap.php
+++ public/importBoostrap.php
@@ -1,9 +1,8 @@
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/bootstrap-theme.css">
 <link rel="stylesheet" href="css/select2.min.css">
 <link rel="stylesheet" href="css/select2-bootstrap.css">
 <link rel="stylesheet" href="css/custom.css">
 <script src="js/bootstrap.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
