907c48ad04621e0a341bf0c7bcbd7b34b4692877
Harsh <hpatel90@illinos.edu>
2015-03-15 20:43:03 -0500

    Removing login modal, replace with nav bar login

--- public/login.php
+++ public/login.php
@@ -94,7 +94,7 @@ ini_set("display_errors", 1);
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="myModalLabel">Login</h4>
+                            <h4 class="modal-title" id="myModalLabel">Quick Login</h4>
                         </div>
                         <div class="modal-body">
                             <?php require_once(dirname(__FILE__).'/../scripts/views/loginView.php'); ?>
