4be82a376f5d0b9ca1e445a76cac06b3783719b9
Olzhas Alipov <alipov@illinois.edu>
2015-03-17 16:18:20 -0500

    added info that describes childstatus constants to childStatus class

--- scripts/models/childStatusEnum.php
+++ scripts/models/childStatusEnum.php
@@ -13,4 +13,25 @@ class childStatus
     const not_here_late = 2; //not here, late
     const not_here_due = 3;  //not here, will arrive later
     const not_here_ok = 4;   //not here, not coming
+
+    const info_here_due = "PARENT IS LATE!"; //here, parent is late
+    const info_here_ok = "CHECKED IN. EXPECTING PARENT.";  //here, should be
+    const info_not_here_late = "LATE!"; //not here, late
+    const info_not_here_due = "ARRIVING.";  //not here, will arrive later
+    const info_not_here_ok = "NOT COMING TODAY.";   //not here, not coming
+
+    public static function getInfo($status = 0){
+        switch ($status) {
+            case self::here_due:
+                return self::info_here_due;
+            case self::here_ok:
+                return self::info_here_ok;
+            case self::not_here_late:
+                return self::info_not_here_late;
+            case self::not_here_due:
+                return self::info_not_here_due;
+            case self::not_here_ok:
+                return self::info_not_here_ok;
+        }
+    }
 }
