a5e4902327980438210bd14e5f5d7f1785ce0ef4
mwallic2 <mwallic@illinois.edu>
2015-03-17 22:56:32 -0500

    Notification message documentation

--- scripts/backgroundTask.php
+++ scripts/backgroundTask.php
@@ -8,14 +8,20 @@ require_once(dirname(__FILE__).'/messageAdapter.php');
 require_once(dirname(__FILE__).'/models/carrierEnum.php');
 require_once(dirname(__FILE__).'/controllers/notificationMessageFactory.php');
 require_once(dirname(__FILE__).'/controllers/notificationMessageController.php');
-/*
+
+/**
+ * Class backgroundTask
+ *
  * This will mimic a chron job locally by providing a single callable iteration of schedule checks.
  */
-
 class backgroundTask
 {
     private $notificationMessageFactory;
 
+    /**
+     * Constructs a backgroundTask
+     * @param notificationMessageFactory $notificationMessageFactory used for dependency injection
+     */
     public function __construct($notificationMessageFactory = null){
         if(!isset($notificationMessageFactory) || $notificationMessageFactory === null){
             $notificationMessageFactory = new notificationMessageFactory();
@@ -24,16 +30,24 @@ class backgroundTask
         $this->notificationMessageFactory = $notificationMessageFactory;
     }
 
+    /**
+     * Sends late and ready notification messages to all parents in db.
+     */
     public function sendEmailsAndTexts(){
         $facilityDao = new FacilityDAO();
         $facilityIds = $facilityDao->findAllFacilityIds();
         $childDao = new childDAO();
         foreach($facilityIds as $id){
-            $this->sendFailityEmails($childDao->findChildrenInFacility($id));
+            $this->sendMessagesToParents($childDao->findChildrenInFacility($id));
         }
     }
 
-    private function sendFailityEmails($children){
+    /**
+     * Sends notification messages to parents of all children.
+     *
+     * @param $children childModel[] the children.
+     */
+    private function sendMessagesToParents($children){
         foreach($children as $child){
             $status = $child->getStatus();
 
