0545648bbba92e9f371bd3e95e7e7f2532a0153a
Harsh Patel <hpatel90@illinois.edu>
2015-04-03 18:16:32 -0500

    Overview comments for errorManager

--- scripts/errorManager.php
+++ scripts/errorManager.php
@@ -1,13 +1,25 @@
 <?php
 
+/**
+ * Class errorManager Defines and allows retrieval of error messages displayed through out the application
+ */
 class errorManager {
 
+    /**
+     * @var array Mapping from error codes to error messages
+     * To add a new error message, add key-value pair below and redirect appropriate page to ?error=key
+     */
     private static $errorMessages = array(
         1 => "Invalid Information",
         2 => "Old Password is Incorrect",
         3 => "New Password and Confirmation Don't Match"
     );
 
+    /**
+     * Retrieves the plain English form of an errorId
+     * @param $errorId The id of the error whose message will be returned
+     * @return string The plain English form of the error
+     */
     public static function getErrorMessage($errorId) {
 
         if (array_key_exists ($errorId, self::$errorMessages)) {
