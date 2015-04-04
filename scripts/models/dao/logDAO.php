fdca1dcade7aba1c736ec12f7bab364cea5134a9
Harsh <hpatel90@illinos.edu>
2015-04-03 20:36:51 -0500

    Adding logDAO

---
+++ scripts/models/dao/logDAO.php
@@ -0,0 +1,34 @@
+<?php
+//require_once(dirname(__FILE__).'/../userModel.php');
+require_once(dirname(__FILE__).'/../db/dbConnectionFactory.php');
+
+class logDAO
+{
+    public static $transLogin = 'Login';
+    public static $transLogout = 'Logout';
+
+    public function findForFacility($facilityID){
+
+    }
+
+
+    public function insert($primaryID, $secondaryID, $transactionType, $additionalInfo) {
+        /*
+         * Should insert into logs table: facilityID (query for this), primaryID, secondaryID, transactionType, addionalInfo, dateTime
+         */
+
+
+//        $connection = DbConnectionFactory::create();
+//        $query = 'INSERT INTO users (email, password, role) VALUES (:email, :password, :role)';
+//        $stmt = $connection->prepare($query);
+//        $stmt->bindParam(':email', $user->email);
+//        $stmt->bindParam(':password', $user->password);
+//        $stmt->bindParam(':role', $user->role);
+//        $stmt->execute();
+//        $id = $connection->lastInsertId();
+//        $connection = null;
+//        return $id;
+
+    }
+
+}
