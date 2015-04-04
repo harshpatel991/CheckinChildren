5a812253c0e36d785ac9d365ea64b7df242a1fc5
pianoajd <pianoajdunk@gmail.com>
2015-04-04 01:28:16 -0500

    Made skeleton unit tests for logDAOTest.

---
+++ tests/unittests/logDAOTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 4/4/15
+ * Time: 1:26 AM
+ */
+
+class logDAOTest extends unitTestBase {
+
+    public function testFindForFacility(){
+
+    }
+
+    public function testInsert(){
+
+    }
+}
