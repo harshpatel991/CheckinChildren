0e4c92aebf51a131df98f0956f99a60357c1a740
pianoajd <pianoajdunk@gmail.com>
2015-04-04 00:23:32 -0500

    Created Skeleton tests for logging so that we can split them up.

---
+++ tests/httptests/tests/LoggingTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 4/4/15
+ * Time: 12:16 AM
+ */
+
+require_once dirname(__FILE__).'/../SeleniumTestBase.php';
+require_once dirname(__FILE__).'/../TestMacros.php';
+
+class LoggingTest extends SeleniumTestBase {
+
+    public function setUp(){
+        parent::setUp();
+    }
+
+    public function testCreateChildLog(){
+
+    }
+
+    public function testCreateParentLog(){
+
+    }
+
+    public function testCreateEmployeeLog(){
+
+    }
+
+    public function testEditEmployeeLog(){
+
+    }
+
+    public function testPromoteEmployeeLog(){
+
+    }
+
+    public function testCheckInCheckOutLog(){
+
+    }
+}
