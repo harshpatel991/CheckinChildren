6d905c9409073dc658979985a9ad29fbbf77bc89
pianoajd <pianoajdunk@gmail.com>
2015-04-03 22:20:38 -0500

    Added logging in check in and check out child form handler, and began in creating the child form handler. Also fixed a bug in the logDAO.

--- scripts/controllers/form_handlers/createChildFormHandler.php
+++ scripts/controllers/form_handlers/createChildFormHandler.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require_once(dirname(__FILE__) . '/../../models/dao/childDAO.php');
+require_once(dirname(__FILE__) . '/../../models/dao/logDAO.php');
 require_once(dirname(__FILE__) . '/../../models/childModel.php');
 require_once(dirname(__FILE__) . '/../../cookieManager.php');
 require_once(dirname(__FILE__) . '/../managerController.php');
@@ -27,6 +28,7 @@ if ($child->isValid()) {
     $child->child_id = $childId;
     $childDAO->update($child);
     header("Location: ../../../public/index.php");
+    $lDAO->insert($_COOKIE[cookieManager::$userId], $child->child_id, $child->child_name, logDAO::$childCreated);
     exit();
 } else {
     header("Location: ../../../public/createChild.php?error=1");
