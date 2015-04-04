190f263f31c8d107210c48dcd0692e5ca90bf0ec
Harsh <hpatel90@illinos.edu>
2015-04-04 01:50:37 -0500

    Unittests for logDAO

--- tests/unittests/logDAOTest.php
+++ tests/unittests/logDAOTest.php
@@ -5,13 +5,35 @@
  * Date: 4/4/15
  * Time: 1:26 AM
  */
-require_once(dirname(__FILE__).'/../../scripts/models/dao/employeeDAO.php');
+require_once(dirname(__FILE__).'/../../scripts/models/dao/logDAO.php');
 require_once(dirname(__FILE__).'/UnitTestBase.php');
 
 class logDAOTest extends unitTestBase {
 
-    public function testFindForFacility(){
+    public function testFindForFacilityID2() {
+        $lDAO = new logDAO();
+        $logs = $lDAO->findForFacility(2, 'time_created');
 
+        $this->assertEquals(2, count($logs));
+        $this->assertEquals("Rick Grimes", $logs[0]["primary_name"]);
+        $this->assertEquals("Bob Dude", $logs[1]["primary_name"]);
+    }
+
+    public function testFindForFacilityID3() {
+        $lDAO = new logDAO();
+        $logs = $lDAO->findForFacility(3, 'time_created');
+
+        $this->assertEquals(1, count($logs));
+        $this->assertEquals("Bob Dude", $logs[0]["primary_name"]);
+    }
+
+    public function testFindForFacilityOrderedByPrimaryName() {
+        $lDAO = new logDAO();
+        $logs = $lDAO->findForFacility(2, 'primary_name');
+
+        $this->assertEquals(2, count($logs));
+        $this->assertEquals("Bob Dude", $logs[0]["primary_name"]);
+        $this->assertEquals("Rick Grimes", $logs[1]["primary_name"]);
     }
 
     public function testInsert(){
