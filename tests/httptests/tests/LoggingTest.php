286048bf0434a2371fbbc4558f25c8cea73f5f9b
pianoajd <pianoajdunk@gmail.com>
2015-04-04 00:50:28 -0500

    Fixed a bug regarding logging in createChildFormHandler. Added a test for logging when creating children.

--- tests/httptests/tests/LoggingTest.php
+++ tests/httptests/tests/LoggingTest.php
@@ -17,6 +17,21 @@ class LoggingTest extends SeleniumTestBase {
 
     public function testCreateChildLog(){
 
+        testMacros::login($this->driver, "manager6@gmail.com", "password6");
+
+        $this->get_element("name=create_child")->click();
+
+        $this->get_element("name=name")->send_keys("Test Case1");
+        $this->get_element("name=PID")->select2("Big Daddy (parent8@gmail.com)");
+        $this->get_element("name=aller")->send_keys("many");
+        $this->get_element("name=submit")->click();
+
+        $this->get_element("name=display_logs")->click();
+
+        $page=$this->driver->get_source();
+
+        $this->assertContains("Test Case1", $page);
+        $this->assertContains("Child Created", $page);
     }
 
     public function testCreateParentLog(){
@@ -37,5 +52,11 @@ class LoggingTest extends SeleniumTestBase {
 
     public function testCheckInCheckOutLog(){
 
+    }
+
+    public function tearDown()
+    {
+        //Any additional teardown goes here.
+        parent::tearDown();
     }
 }
