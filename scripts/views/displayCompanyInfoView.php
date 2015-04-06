e3166fc26e0b780587ab53e36f07156e8dfe8643
Elzabad Kennedy <kenned45@illinois.edu>
2015-04-05 21:31:13 -0500

    Implemented view the company's profile

---
+++ scripts/views/displayCompanyInfoView.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+
+require_once(dirname(__FILE__).'/../controllers/authController.php');
+require_once(dirname(__FILE__).'/../models/dao/companyDAO.php');
+require_once(dirname(__FILE__).'/../models/companyModel.php');
+
+$companyDAO=new companyDAO();
+$company=$companyDAO->find($_COOKIE[cookieManager::$userId]);
+?>
+
+<h1>Account Profile</h1>
+<table id="company_info_table" class="table">
+    <tr>
+        <th>Name</th>
+        <td><?php echo $company->company_name; ?></td>
+    </tr>
+    <tr>
+        <th>Address</th>
+        <td><?php echo $company->address; ?></td>
+    </tr>
+    <tr>
+        <th>Phone Number</th>
+        <td><?php echo $company->phone; ?></td>
+    </tr>
+</table>
+
+<br>
+<a class="btn btn-success" id="edit_parent" href="editCompany.php"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit Information</a>
+<a class="btn btn-success" id="update_password" href="updatePassword.php"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Change Password</a>
+
+<hr>
+
+<a id="home" href="index.php" class="btn btn-primary"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Home</a>
+
+
--- scripts/views/sideBarView.php
+++ scripts/views/sideBarView.php
@@ -16,6 +16,7 @@
         <a href="createFacility.php" name="create_facilities" class="list-group-item"><span class="glyphicon glyphicon-plus-sign"" aria-hidden="true"></span> Create A Facility</a></li>
         <a href="displayManagers.php" name="view_managers" class="list-group-item"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> View My Managers</a></li>
         <a href="createManager.php" name="create_managers" class="list-group-item"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Create A Manager</a></li>
+        <a href="displayCompanyInfo.php" name="view_company_profile" class="list-group-item"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Company Profile</a></li>
     <?php } else if ($_COOKIE[cookieManager::$userRole]=='parent') { ?>
         <a href="displayParentInfo.php" name="view_parent_profile" class="list-group-item"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> My Profile</a></li>
         <a href="displayChildren.php" id="view_my_children" class="list-group-item"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> My Children</a></li>
@@ -29,6 +30,7 @@ function employeeSideBar() { ?>
     <a href="checkinChildren.php" name="view_children" class="list-group-item"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> View Children</a>
     <a href="createParent.php" name="create_parent" class="list-group-item"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Create Parent</a>
     <a href="createChild.php" name="create_child" class="list-group-item"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Create Child</a>
+    <a href="updatePassword.php" name="change_password" class="list-group-item"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Change Password</a></li>
 <?php
 }
 ?>
