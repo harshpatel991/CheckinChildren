cbe5e8719a6d4c90c80082d0430554341f264fde
Alex Dahlquist <pianoajdunk@gmail.com>
2015-04-06 21:11:44 -0500

    Added comments to login and facility controllers.

--- scripts/controllers/facilityController.php
+++ scripts/controllers/facilityController.php
@@ -1,14 +1,24 @@
 <?php
-
+/**
+ * This classs is called by the facility view in order to access the facility DAO objects.
+ */
 require_once(dirname(__FILE__).'/../models/dao/facilityDAO.php');
 
 class facilityController {
 
+    /**
+     * @param $companyId int The id of the company whose facility we wish to retrieve
+     * @return array An array of facilities who the company manages
+     */
     public function getAllFacilities($companyId) {
         $facilityDAO = new FacilityDAO();
         return $facilityDAO->findFacilitiesInCompany($companyId);
     }
 
+    /**
+     * @param $facilityId int Gets the facility information by this id
+     * @return mixed Returns the facility info if the id is valid, and 0 if it does not
+     */
     public function getFacility($facilityId) {
         $facilityDAO = new FacilityDAO();
         return $facilityDAO->find($facilityId);
