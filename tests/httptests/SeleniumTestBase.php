bc3467a85c79cb19f1853da2c76ae301db00862e
mwallic2 <mwallic@illinois.edu>
2015-04-09 15:46:17 -0500

    Change Selenium tests to 1 retry

--- tests/httptests/SeleniumTestBase.php
+++ tests/httptests/SeleniumTestBase.php
@@ -15,7 +15,7 @@ require_once dirname(__FILE__).'/../../config.php';
 class SeleniumTestBase extends PHPUnit_Framework_TestCase {
   protected $driver;
   protected $dbConn;
-  protected $retries = 0;
+  protected $retries = 1;
 
   public static $baseUrl = "";
   public static $isWindows = false;
