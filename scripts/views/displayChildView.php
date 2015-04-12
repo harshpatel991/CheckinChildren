618ce5d3e57eff9b1d8614e2badc5555cb8e7091
mwallic2 <mwallic@illinois.edu>
2015-04-12 06:00:26 -0500

    Child history

--- scripts/views/displayChildView.php
+++ scripts/views/displayChildView.php
@@ -2,11 +2,13 @@
 
 require_once(dirname(__FILE__).'/../cookieManager.php');
 require_once(dirname(__FILE__).'/../models/dao/childDAO.php');
+require_once(dirname(__FILE__).'/../models/dao/logDAO.php');
 require_once(dirname(__FILE__).'/../models/childModel.php');
 require_once(dirname(__FILE__).'/../dateTimeProvider.php');
 require_once(dirname(__FILE__).'/../models/childStatusEnum.php');
 
 $childDAO = new childDAO();
+$logDao = new logDAO();
 
 if(isset($_GET['child_id'])) {
     $child = $childDAO->find($_GET['child_id']);
@@ -37,30 +39,72 @@ if(isset($_GET['child_id'])) {
     </table>
 
 
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th>Day</th>
-            <th>Checkin</th>
-            <th>Checkout</th>
-        </tr>
-        </thead>
-        <tbody>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4  data-toggle="collapse" data-target="#timesPanel" aria-expanded="true" class="">Checkin/Out Times</h4>
+        </div>
+        <div id="timesPanel" class="panel-collapse collapse" aria-expanded="true">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Day</th>
+                    <th>Checkin</th>
+                    <th>Checkout</th>
+                </tr>
+                </thead>
+                <tbody>
 
-        <?php
-        $days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-        $ci = $child->expectCheckinReadable();
-        $co = $child->expectCheckoutReadable();
-        for ($i=0; $i<7; $i++){
-            ?>
-            <tr>
-                <td><?php echo $days[$i]; ?></td>
-                <td><?php echo $ci[$i]; ?></td>
-                <td><?php echo $co[$i]; ?></td>
-            </tr>
-        <?php }?>
-        </tbody>
-    </table>
+                <?php
+                $days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+                $ci = $child->expectCheckinReadable();
+                $co = $child->expectCheckoutReadable();
+                for ($i=0; $i<7; $i++){
+                    ?>
+                    <tr>
+                        <td><?php echo $days[$i]; ?></td>
+                        <td><?php echo $ci[$i]; ?></td>
+                        <td><?php echo $co[$i]; ?></td>
+                    </tr>
+                <?php }?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4  data-toggle="collapse" data-target="#historyPanel" aria-expanded="true" class="">History</h4>
+        </div>
+        <div id="historyPanel" class="panel-collapse collapse" aria-expanded="true">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Event</th>
+                    <th>Employee</th>
+                    <th>Other</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php
+                $childEvents = $logDao->getChildHistory($child->child_id, 10);
+                foreach ($childEvents as $event){
+                    $dateTime = dateTimeProvider::getdate($event->time_created);
+                    ?>
+                    <tr>
+                        <td><?php echo dateTimeProvider::readableDate($dateTime); ?></td>
+                        <td><?php echo dateTimeProvider::readableTime($dateTime, false); ?></td>
+                        <td><?php echo $event->transaction_type; ?></td>
+                        <td><?php echo $event->primary_name; ?></td>
+                        <td><?php echo $event->additional_info; ?></td>
+                    </tr>
+                <?php }?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <a class="btn btn-success" name="edit_child" href="editChild.php?child_id=<?php echo $_GET['child_id']; ?>"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit This Child</a>
     <hr>
