2fa4c4cd0bfa3d10b0a88f50236a48b708ad51ec
Alex Dahlquist <pianoajdunk@gmail.com>
2015-04-20 20:57:58 -0500

    There was some tomfoolery in the tests. Looks like a copy paste error occured in company test. An old auth controller test had to be updated due to new functionality.

--- tests/unittests/authControllerTest.php
+++ tests/unittests/authControllerTest.php
@@ -161,7 +161,7 @@ class authControllerTest extends unitTestBase
             array(8, 1, false),
             array(8, 1, false),
             array(1, 1, false),
-            array(1, 2, false),
+            array(1, 2, true),
             array(1, 999, false),
         );
     }
